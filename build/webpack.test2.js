e0dbb8754564c3ca09e8c38809275dbbb3f272b3
Stony Chen <stonychen@hotmail.com>
2020-07-08 21:19:46 +0800

    ut

--- build/webpack.test2.js
+++ build/webpack.test2.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const nodeExternals = require('webpack-node-externals')
+const WebpackShellPlugin = require('webpack-shell-plugin')
 
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
+  entry: './tests/unit/all-tests.ts',
   devtool: 'inline-cheap-module-source-map',
   // node: {
   //   fs: 'empty'
@@ -12,10 +14,16 @@ module.exports = merge(common, {
   stats: "errors-only",
   externals: [nodeExternals()],
   output: {
+    filename: 'all-tests.js',
     path: path.resolve(process.cwd(), 'dist'),
     // ...
     // use absolute paths in sourcemaps (important for debugging via IDE)
     devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
-  }
+  },
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildExit: "mocha ./dist/all-tests.js"
+    })
+  ]
 })
